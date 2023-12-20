7911d9dc42994726f47e2720af02087d0f96b8b4
owusuvictorjnr <owusuvictor80@gmail.com>
2023-12-20 07:30:37 +0000

    clerk sign-in button

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,34 @@
+import { SignedOut } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from '../ui/button'
+
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <header className="w-full border-b">
+      <div className="wrappper flex items-center justify-between mx-5">
+        <Link href="/" className="w-36 flex items-center capitalize gap-1">
+          <Image
+            src="/assets/images/logo.jpg"
+            alt="logo"
+            width={128}
+            height={38}
+            className="h-[3rem] w-[3rem] rounded-full"
+          />
+          <h1 className="">eventsHome</h1>
+        </Link>
+
+        <div className="flex w-32 justify-end gap-3">
+          {/* using clerk  */}
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
