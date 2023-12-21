574245071ca5b0491ab1211e8879d13fc2e2a48f
owusuvictorjnr <owusuvictor80@gmail.com>
2023-12-21 09:13:56 +0000

    container for the sidenar

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,7 +1,51 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+import Image from 'next/image'
+import { Separator } from '../ui/separator'
+import NavItems from './NavItems'
 
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
+    <nav className="md:hidden">
+      <Sheet>
+        <SheetTrigger className="align-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            data-slot="icon"
+            className="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25"
+            />
+          </svg>
+        </SheetTrigger>
+        <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+          <Image
+            src="/assets/images/logo.jpg"
+            alt="logo"
+            height={38}
+            width={128}
+            className="h-[3rem] w-[3rem] rounded-full"
+          />
+
+          <Separator className="border border-gray-50" />
+
+          <NavItems />
+        </SheetContent>
+      </Sheet>
+    </nav>
   )
 }
 
